2d9600f8e0f1033315aebe4adcd708d8a79cece6
FLU <valerio.massj@gmail.com>
2021-11-05 11:40:05 +0100

    creato un array di 7 oggetti

---
+++ script.js
@@ -0,0 +1,45 @@
+let bikes = [
+    {
+        "nome": "ultrabike",
+        "peso": 12,
+        "brand": "yamaha"
+    },
+
+    {
+        "nome": "megabike",
+        "peso": 15,
+        "brand": "camaro"
+    },
+
+    {
+        "nome": "mountain",
+        "peso": 17,
+        "brand": "ferrari"
+    },
+
+    {
+        "nome": "deca",
+        "peso": 10,
+        "brand": "decatlon"
+    },
+
+    {
+        "nome": "magnifica",
+        "peso": 17.5,
+        "brand": "lamborghini"
+    },
+
+    {
+        "nome": "gigachad",
+        "peso": 25,
+        "brand": "chad inc."
+    },
+
+    {
+        "nome": "minibike",
+        "peso": 7,
+        "brand": "child e co."
+    }
+]
+
+console.log(bikes);
